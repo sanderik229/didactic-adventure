429ea7159a83940c5f2fe4a343caff0caddc5d02
Левон Бабаян <levonbabaan@MacBook-Air-Levon.local>
2023-09-14 09:55:48 +0300

    отправляем

---
+++ прп/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("сколько ложек сахара вы хотите?");
+string txt = Console.ReadLine();
+int num = Convert.ToInt32(txt);
+int kolvolojek = 0;
+while (kolovlojek !=num+1);
+{
+    Console.WriteLine("добавили " + kolvolojek + " ложек в чай");
+    kolvolojek = kolvolojek + 1;
+    kolvolojek += 1;*/;
+    kolovlojek++;
+}
